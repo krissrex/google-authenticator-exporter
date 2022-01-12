b4568c34459e864b74402550a3448589185d8cc8
Kristian Rekstad <kre@capraconsulting.no>
2022-01-12 09:21:22 +0100

    Handle missing issuer and name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 /**
  * Google Authenticator uses protobuff to encode the 2fa data.
- * 
- * @param {Uint8Array} payload 
+ *
+ * @param {Uint8Array} payload
  */
 function decodeProtobuf(payload) {
   const protobuf = require("protobufjs");
@@ -21,12 +21,12 @@ function decodeProtobuf(payload) {
 }
 
 /**
- * Convert a base64 to base32. 
- * Most Time based One Time Password (TOTP) 
+ * Convert a base64 to base32.
+ * Most Time based One Time Password (TOTP)
  * password managers use this as the "secret key" when generating a code.
- * 
+ *
  * An example is: https://totp.danhersam.com/.
- * 
+ *
  * @returns RFC3548 compliant base32 string
  */
 function toBase32(base64String) {
@@ -39,14 +39,14 @@ function toBase32(base64String) {
  * The data in the URI from Google Authenticator
  *  is a protobuff payload which is Base64 encoded and then URI encoded.
  * This function decodes those, and then decodes the protobuf data contained inside.
- * 
+ *
  * @param {String} data the `data` query parameter from the totp migration string that google authenticator outputs.
  */
 function decode(data) {
   const buffer = Buffer.from(decodeURIComponent(data), "base64");
 
   const payload = decodeProtobuf(buffer);
-  
+
   const accounts = payload.otpParameters.map(account => {
     account.totpSecret = toBase32(account.secret);
     return account;
@@ -58,7 +58,7 @@ function decode(data) {
 /**
  * Write the json with account information to a file
  * so it can be uploaded to other password managers etc easily.
- * 
+ *
  * @param {String} data A `JSON.stringify`ed list of accounts.
  */
 function saveToFile(filename, data) {
@@ -75,23 +75,23 @@ function saveToFile(filename, data) {
  * @param accounts A list of the auth accounts
  */
 function saveToQRCodes(accounts){
-  
+
   const QRCode = require('qrcode')
   const fs = require("fs");
-  
+
   const directory = "./qrCodes"
   if(!fs.existsSync(directory)){
     fs.mkdirSync(directory)
   }
 
   accounts.forEach(account => {
-    const name = account.name
-    const issuer = account.issuer
+    const name = account.name || ""
+    const issuer = account.issuer || ""
     const secret = account.totpSecret
-    
+
     const url = `otpauth://totp/${encodeURI(name)}?secret=${encodeURI(secret)}&issuer=${encodeURI(issuer)}`
-    const file = `${directory}/${issuer}(${name}).png`
-  
+    const file = `${directory}/${issuer || "No issuer"} (${name}).png`
+
     if(fs.existsSync(file)) {
       console.log(`${file.yellow} already exists.`)
     }else{
@@ -102,7 +102,7 @@ function saveToQRCodes(accounts){
         console.log(`${file.green} created.`)
       })
     }
-    
+
   })
 }
 
@@ -114,7 +114,7 @@ function saveToQRCodes(accounts){
 function toJson(filename, saveToFile, accounts) {
   console.log(filename)
   console.log(saveToFile)
-  
+
   if (saveToFile && filename) {
     console.log(`Saving to "${filename}"...`);
     saveToFile(filename, JSON.stringify(accounts, undefined, 4));
@@ -147,7 +147,7 @@ function promptUserForUri() {
   // I took a picture of the QR with my camera, because Google Authenticator prevents screenshots.
   // I then uploaded the picture to my pc via the SD-card and scanned with my phone...
   // Very many steps
-  
+
   const resultType = {
     QRCODE: "qrcode",
     JSON: "json",
@@ -169,9 +169,9 @@ function promptUserForUri() {
     const uri = result.totpUri;
     const queryParams = new URL(uri).search;
     const data = new URLSearchParams(queryParams).get("data");
-    
+
     const accounts = decode(data);
-    
+
     switch(mode){
       case resultType.QRCODE:
         saveToQRCodes(accounts)
